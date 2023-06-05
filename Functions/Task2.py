7e933873d75f941a673530106dd67a7fed9920f8
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-06-05 23:02:10 +0500

    Done Functions

--- Functions/Task2.py
+++ Functions/Task2.py
@@ -1,9 +1,22 @@
 ﻿def union(*args) -> set:
+    result = set()
     for i in range(len(args)):
-        for k in args:
-            print(k)
+        for k in args[i]:
+            result.add(k)
+    return result
             
 print(union(('S', 'A', 'M'), ['S', 'P', 'A', 'C']))
-def intersect(*args) -> set:
-    raise NotImplementedError("Implement me!")
 
+def intersect(*args) -> set:
+    result = set()
+    if len(args) == 1:
+        return set(args)
+    else:
+        for i in range(len(args)):
+            for k in range(1, len(args)):
+                result = set(args[i]).intersection(set(args[k]))
+            return result
+
+print(intersect(('S', 'A', 'C'), ('P', 'C', 'S'), ('G', 'H', 'S', 'C')))
+print(intersect(('S', 'A'), ('P', 'C'), ('G', 'H')))
+print(intersect(['S', 'A', 'M'], ['S', 'P', 'A', 'M', 'C']))
--- Functions/Task3.py
+++ Functions/Task3.py
@@ -1,1 +1,14 @@
-﻿
+﻿from collections import defaultdict
+
+def combine_dicts(*args):
+    result = defaultdict(int)
+    for dictionary in args:
+        for key, value in dictionary.items():
+            result[key] += value
+    return dict(result)
+
+dict_1 = {'a': 100, 'b': 200}
+dict_2 = {'a': 200, 'c': 300}
+dict_3 = {'a': 300, 'd': 100}
+
+print(combine_dicts(dict_1, dict_2, dict_3))
