9883145607c84185ed9a50e946801104aed59065
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-06-08 00:17:01 +0500

    Recursions

---
+++ Recursion/Task1.py
@@ -0,0 +1,14 @@
+﻿def seq_sum(sequence):
+    result = 0
+    for i in sequence:
+        if isinstance(i, int):
+            result += i
+        elif isinstance(i, list): # elif isinstance(i, list or tuple):
+            result += seq_sum(i)
+        elif isinstance(i, tuple):
+            result += seq_sum(i)
+    return result
+  
+sequence = [1,2,3,[4,5, (6,7)]]
+
+print(seq_sum(sequence))
---
+++ Recursion/Task2.py
@@ -0,0 +1,16 @@
+﻿
+def linear_seq(sequence):
+    result = []
+    for i in sequence:
+        if isinstance(i, int):
+            result.append(i)
+        elif isinstance(i, list): # elif isinstance(i, list or tuple):
+            result.extend(linear_seq(i))
+        elif isinstance(i, tuple):
+            result.extend(linear_seq(i))
+    return result
+  
+sequence = [1,2,3,[4,5, (6,7)]]
+
+print(linear_seq(sequence))
+[1,2,3,4,5,6,7]
