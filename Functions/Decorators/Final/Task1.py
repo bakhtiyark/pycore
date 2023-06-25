cc6c6ffd9012d69d148a12b005d3421a55dbabf8
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-06-25 19:06:29 +0500

    further

--- Functions/Decorators/Final/Task1.py
+++ Functions/Decorators/Final/Task1.py
@@ -3,7 +3,21 @@ Implement a function that works the same as the str.split method (without using 
 
 Example:
 
-    def split(data: str, sep=None, maxsplit=-1):
-        ...
+   
         
 """
+def split(data: str, sep=None, maxsplit=-1):
+    part = ""
+    result = []
+    for i in data:
+        if i != sep:
+            part += i
+        elif part != "":
+            result.append(part)
+        
+    return(result)
+    
+
+print(split("I am a motherfucker"," "))
+print(split(",123,",","))
+print(split(""))
