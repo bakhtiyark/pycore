2f3b81d49ff113f625d9de05d32fd05ea01f28d3
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-06-26 21:28:13 +0500

    dd

--- Functions/Decorators/Final/Task1.py
+++ Functions/Decorators/Final/Task1.py
@@ -4,23 +4,41 @@ Implement a function that works the same as the str.split method (without using 
 Example:
 
    
-        
+        for char in firstsentence:
+    if char == " ":
+        words.append(current_word)
+        current_word = ""
+    else:
+        current_word += char
 """
 def split(data: str, sep=None, maxsplit=-1):
     part = ""
     result = []
-    split_counter = maxsplit
+    if(len(data)==0):
+        return result
+    if(sep == None):
+        sep=" "
     for i in data:
-        if i != sep:
-            part += i
-        elif part != sep:
-            result.append(sep)
+        if i == sep:
             result.append(part)
-            
-    return(result)
+            part = ""
+        else:
+            part+=i
+                
+    result.append(part)
+    if(maxsplit==-1):
+        return(result)
+    else:
+        return(result[0:maxsplit+1])
     
 
-print(split("I am a motherfucker"," "))
+print(split("I am a motherfucker")) #check
+print(split("adf<>5","<>", maxsplit=0))
+print(split(",123,",","))
+print(split(",123,",","))
 print(split(",123,",","))
 print(split(""))
-print('1,2,3'.split(',', maxsplit=1))
+print(split(''))
+print(split("adf 5 7")) #check
+print(split("adf 5  7",maxsplit=1)) #check
+print(split("la-la-la-la","-",maxsplit=3))
