ba84cdb980760df879d1b9902745da1d12047fa0
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-02 22:17:54 +0500

    task 2 oop

---
+++ OOP/Exceptions/Task2.py
@@ -0,0 +1,18 @@
+﻿from typing import Union
+
+
+def divide(str_with_ints: str) -> Union[float, str]:
+    try:
+        intermediate = str_with_ints.split()
+        return int(intermediate[0])/int(intermediate[1])
+    except ValueError as a:
+          return f"Error code: {a}"
+    except ZeroDivisionError:
+          return "Error code: division by zero"
+    
+print(divide("1 2"))
+print(divide("1 0"))
+print(divide("* 2"))
+print(divide("ddad 2"))
+print(divide("4 *"))
+print(divide("4 %"))
