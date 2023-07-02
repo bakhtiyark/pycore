475a884575fda6400c389641edcee43d803dd560
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-02 21:31:12 +0500

    dd

--- Functions/Decorators/Final/Task1.py
+++ Functions/Decorators/Final/Task1.py
@@ -11,34 +11,67 @@ Example:
     else:
         current_word += char
 """
-def split(data: str, sep=None, maxsplit=-1):
-    part = ""
+def custom_split(string, separator=None, maxsplit=-1):
+    if separator is None:
+        separator = " "
+    
     result = []
-    if(len(data)==0):
-        return result
-    if(sep == None):
-        sep=" "
-    for i in data:
-        if i == sep:
-            result.append(part)
-            part = ""
-        else:
-            part+=i
-                
-    result.append(part)
-    if(maxsplit==-1):
-        return(result)
-    else:
-        return(result[0:maxsplit+1])
+    start = 0
+    count = 0
+    if maxsplit == 0:
+        return []
+
+    while maxsplit != count:
+        index = string.find(separator, start)
+        if index == -1:
+            break
+        
+        result.append(string[start:index])
+        start = index + len(separator)
+        count += 1
+
+    result.append(string[start:])
+    return result
+
+
+
+
+
+def split(data: str, sep=None, maxsplit=-1):
+    if sep is None:
+        sep = " "
+    
+    result = []
+    start = 0
+    count = 0
+    if bool(data) == False:
+        return []
+    stripped_data = data.strip()
+    while maxsplit != count:
+        index = stripped_data.find(sep, start)
+        if index == -1:
+            break
+        
+        result.append(stripped_data[start:index])
+        start = index + len(sep)
+        count += 1
+
+    result.append(stripped_data[start:])
+    return result
     
 
-print(split("I am a motherfucker")) #check
-print(split("adf<>5","<>", maxsplit=0))
+print(split("I am a motherfucker")) #expect
+print(split("adf<>5","<>", maxsplit=0)) # ['adf<>5']'
+print(split('adf 5')) # ['adf', '5']
+print(split(' asdf 5 7', maxsplit=0)) 
+print(split(' asdf 5 7', maxsplit=1))
 print(split(",123,",","))
-print(split(",123,",","))
-print(split(",123,",","))
-print(split(""))
-print(split(''))
-print(split("adf 5 7")) #check
+print(split("")) # expect []
+print(split('', '')) # expect []
+print(split(' ',","))
+print(split('')) # expect []
+print(split("adf 5 7", maxsplit=1)) #check
+print(split('adf<>5asdf<>aasdf',"<>", maxsplit=1)) # expect 
 print(split("adf 5  7",maxsplit=1)) #check
-print(split("la-la-la-la","-",maxsplit=3))
+print(split("la-la-la-la","-",maxsplit=1))
+print(bool(""))
