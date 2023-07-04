c254c42a797e5937d93956383f95666572f4bb1f
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-04 12:00:53 +0500

    Function decorator task1

--- Functions/Decorators/Final/Task1.py
+++ Functions/Decorators/Final/Task1.py
@@ -19,20 +19,6 @@
 
     result.append(stripped_data[start:])
     return result
-    
 
-print(split("I am a motherfucker")) #expect
-print(split("adf<>5","<>", maxsplit=0)) # ['adf<>5']'
-print(split('adf 5')) # ['adf', '5']
-print(split(' asdf 5 7', maxsplit=0)) 
-print(split(' asdf 5 7', maxsplit=1))
-print(split(",123,",","))
-print(split("")) # expect []
-print(split('', '')) # expect []
-print(split(' ',","))
-print(split('')) # expect []
-print(split("adf 5 7", maxsplit=1)) #check
-print(split('adf<>5asdf<>aasdf',"<>", maxsplit=1)) # expect 
-print(split("adf 5  7",maxsplit=1)) #check
-print(split("la-la-la-la","-",maxsplit=1))
-print(bool(""))
+print(split(' adf 5 7'))
+print(split(' adf 5 7',maxsplit=1))
