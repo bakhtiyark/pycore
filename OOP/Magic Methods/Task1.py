c1dbed3d01dfe4f23336bc87fb7e5e43256ad050
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-04 21:14:17 +0500

    OOP

---
+++ OOP/Magic Methods/Task1.py
@@ -0,0 +1,17 @@
+﻿from typing import List
+
+
+class Counter:
+    def __init__(self, values: List[int]):
+        self.values = values
+    def __add__(self, addition):
+        result = []
+        for i in self.values:
+            result.append(str(i) +" " + addition)
+        return result
+
+objet = Counter([1,2,3]) + "Real mofo"
+print(objet)
+kk = Counter([1,2])
+print(Counter)
+print(kk)
---
+++ OOP/Magic Methods/Task2.py
@@ -0,0 +1,39 @@
+﻿class Bird:
+    def __init__(self, name):
+        self.name = name
+
+    def walk(self):
+        return f"{self.name} bird can walk"
+
+    def fly(self):
+        return f"{self.name} bird can fly"
+
+
+class FlyingBird(Bird):
+    def __init__(self, name, ration="grains"):
+        super().__init__(name)
+        self.ration = ration
+
+    def eat(self):
+        return f"It mostly eats {self.ration}"
+
+
+class NonFlyingBird(FlyingBird):
+    def __init__(self, name, ration="grains"):
+        super().__init__(name)
+        self.ration = ration
+    def swim(self):
+        return f"{self.name} bird can swim"
+
+class SuperBird(NonFlyingBird):
+    def __init__(self, name, ration="grains"):
+        super().__init__(name)
+        self.ration = ration
+
+
+tit = Bird("Great Tit")
+robin = FlyingBird("Robin")
+penguin = NonFlyingBird("Penguin")
+cthulhu = SuperBird("Cthulhu")
+print(tit.walk())
+print(robin.eat())
