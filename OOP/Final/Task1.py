8c3e73b83346d37196777f0e86f87fe36de0f09d
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-09 18:43:25 +0500

    placeholder for finals

---
+++ OOP/Final/Task1.py
@@ -0,0 +1,8 @@
+﻿"""A singleton is a class that only allows a single instance of itself to be created and gives access to that created instance. Implement singleton logic inside your custom class using a method to initialize a class instance.
+
+Example:
+
+>>> p = Sun.inst()
+>>> f = Sun.inst()
+>>> p is f
+True"""
---
+++ OOP/Final/Task2.py
@@ -0,0 +1,32 @@
+﻿"""Implement the keyword encoding and decoding for the Latin alphabet. The keyword cipher uses a keyword to rearrange the letters in the alphabet. You should add the provided keyword at the beginning of the alphabet. A keyword is used as the key, which determines the letter matchings of the cipher alphabet to the plain alphabet. The repeats of letters in the word are removed, then the cipher alphabet is generated with the keyword matching to A, B, C, etc. until the keyword is used up, whereupon the rest of the ciphertext letters are used in alphabetical order, excluding those already used in the key.
+
+Encryption:
+
+The keyword is "Crypto"
+
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+C R Y P T O A B D E F G H I J K L M N Q S U V W X Z
+Example:
+
+>>> cipher = Cipher("crypto")
+>>> cipher.encode("Hello world")
+"Btggj vjmgp"
+
+>>> cipher.decode("Fjedhc dn atidsn")
+"Kojima is genius"""
+
+
+class Cipher:
+    original = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    substitution = ['C', 'R', 'Y', 'P', 'T', 'O', 'A', 'B', 'D', 'E', 'F', 'G',
+                    'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Q', 'S', 'U', 'V', 'W', 'X', 'Z']
+
+    def __init__(self) -> None:
+        pass
+
+    def encode(str):
+        pass
+
+    def decode(str):
+        pass
