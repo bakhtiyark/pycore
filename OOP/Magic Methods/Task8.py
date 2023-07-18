727423360b75a7ecabd53fa2417b56bffae3ce31
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-18 20:07:51 +0500

    d

--- OOP/Magic Methods/Task8.py
+++ OOP/Magic Methods/Task8.py
@@ -1,18 +1,5 @@
-﻿pencil_number = int(input("How many pencils would you like to use:"))
-name_x = "John"
-name_y = "Jack"
-first_person = input(f"Who will be the first ({name_x}, {name_y}):")
-print("|" * pencil_number)
-while(pencil_number>0):
-    if(first_person !=  name_x):
-        print(name_y + "'s turn:")
-        removal = int(input())
-        pencil_number -= removal
-        print("|" * pencil_number)
-        first_person = name_x
-    else:
-        print(name_x + "'s turn:")
-        removal = int(input())
-        pencil_number -= removal
-        print("|" * pencil_number)
-        first_person = name_y
+﻿import hashlib
+
+lala = hashlib.new("sha384",b'LALALA').hexdigest()
+
+print(lala)
