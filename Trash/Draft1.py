9c51569f59646d386086db067cef548febafc2cc
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-07-23 21:10:03 +0500

    ff

---
+++ Trash/Draft1.py
@@ -0,0 +1,19 @@
+﻿# Simple program to find square root of a number
+def square_root1(number):
+    for i in range(1, number):
+        if(i * i > number):
+            return i - 1
+
+# More efficient way to find square root of a number
+def square_root(number):
+    low = 0 
+    high = number
+    while(low <= high):
+        mid = (low + high) // 2
+        if(mid * mid > number):
+            high = mid - 1
+        else:
+            low = mid + 1
+    return high
+
+print(square_root(2400))
